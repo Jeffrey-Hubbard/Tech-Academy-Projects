d474df59160d7c2f9f262b4517b25743ffc818f8
Jeffrey Hubbard <jeffrey.hubbard@gmail.com>
2018-05-19 20:33:31 -0700

    Commit Visual Studio page with content changes.

---
+++ Visual_Studio_Test/Visual_Studio_Test/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Visual_Studio_Test.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}
